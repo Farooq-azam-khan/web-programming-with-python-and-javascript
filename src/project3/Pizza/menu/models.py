edfab16408edd9db72a4fa57c435089b128a9eca
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-07-18 05:02:55 -0400

    todo: create proper models for menu and user

--- src/project3/Pizza/menu/models.py
+++ src/project3/Pizza/menu/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
+
+class PizzaOrder(models.Model):
+    sicilian = models.BooleanField()
+    large = models.BooleanField()
+    toppings = models.ManyToManyField(Topping, blank=True, related_name="toppings")
+    topping_number = models.IntegerField()
+    
+    def price(self):
+        return 0.0
+        
+    def valid_topping_number(self):
+        return self.topping_number > 0 and self.topping_number < 5
+    def valid_toppings(self):
+        return self.toppings.count() > self.topping_number or self.toppings.count() < self.topping_number
+
+class SubsOrder(models.Model):
+    name = models.CharField()
+    large = models.BooleanField(blank=True)
+    
+    def price(self):
+        return 0.0
+        
+    def __str__(self):
+        return f''
+    
+    
+
+class PastaOrder(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    price = models.DecimalField(blank=False, null=False)
+    
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+class SaladsOrder(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    price = models.DecimalField(blank=False, null=False)
+    
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+class DinnerPlattersOrder(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    large = models.BooleanField(blank=False)
+    def price(self):
+        return 0.0
+        
+    def __str__(self):
+        return f'{self.name} {self.size}'
+        
--- src/project3/Pizza/user/models.py
+++ src/project3/Pizza/user/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from menu.models import (Topping,
+                        PizzaOrder, 
+                        SubsOrder,
+                        PastaOrder,
+                        SaladsOrder,
+                        DinnerPlattersOrder)
+                        
 # Create your models here.
+class User(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    pizza  = models.ForeignKey(PizzaOrder, on_delete=models.CASCADE, related_name='pizza')
+    subs  = models.ForeignKey(SubsOrder, on_delete=models.CASCADE, related_name='subs')
+    pasta = models.ForeignKey(PastaOrder, on_delete=models.CASCADE, related_name='pasta')
+    salads = models.ForeignKey(SaladsOrder, on_delete=models.CASCADE, related_name='salads')
+    dinner = models.ForeignKey(DinnerPlattersOrder, on_delete=models.CASCADE, related_name='dinner')
+    
+    def total_price(self):
+        return 10.0
+        
+    def __str__(self):
+        orders = [self.pizza, self.subs, self.pasta, self.salads, self.dinner]
+        ret = f'{self.user} order: '
+        for order in orders:
+            if order:
+                ret+= f'{order}'
+        return ret
